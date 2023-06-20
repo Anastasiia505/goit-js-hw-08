cbc494b543530326d855725b50ae1e140d7a1d59
Anastasiia505 <nastyonochka07@gmail.com>
2023-06-20 08:56:43 +0300

    hw-8-import

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
-// Add imports above this line
+//Add imports above this line
+
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galerryEl = document.querySelector(".gallery");
+const createGallery = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`);
+galerryEl.insertAdjacentHTML('beforeend', createGallery.join(''));
+galerryEl.addEventListener('click', onClick);
+function onClick(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+        return;
+ }
+    const instance = basicLightbox.create
+      (`<img src="${e.target.dataset.source} "width=800", "height=600">`, {
+        onShow: (instance) => document.addEventListener('keydown', onKeydown),
+        onClose: (instance)=>document.removeEventListener('keydown', onKeydown)
+        });
+    
+    instance.show();
+    
+    function onKeydown(e) {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  }
+
+}

8eee25c7da9be60d0106baefac66cc1319aea813
Anastasiia505 <nastyonochka07@gmail.com>
2023-06-22 01:06:18 +0300

    hw-8-kfkkf

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate',
+throttle(function (e) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(e));
+}, 1000)
+);
+
+const valueSecond = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+console.log(valueSecond);
+player.setCurrentTime(valueSecond.seconds).then(function(second) {
+  
+}).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+    
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
 
+formEl = document.querySelector('.feedback-form');
+textAreaEl = document.querySelector('textarea');
+emailEl=document.querySelector('input[name="email"]')
+// formEl = addEventListener('submit', onFormSubmit);
+
+formEl = addEventListener("input", onFormInput);
+const formData = {};
+
+function onFormInput(e) {
+
+    formData.email = emailEl.value;
+    formData.message = textAreaEl.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+    
+}
+       
